ba8d6f1ac464c37d4ec319cf2f422534fab61a39
Mohamedeid602 <eidmedo215@gmail.com>
2025-04-05 20:20:29 +0200

    Statiitics page

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { ProcessService } from '../services/process.service';
 
 @Component({
-  selector: 'app-statistics',
-  imports: [],
+  selector: 'app-dashboard',
   templateUrl: './statistics.component.html',
-  styleUrl: './statistics.component.css'
+  styleUrls: ['./statistics.component.css']
 })
-export class StatisticsComponent {
+export class StatisticsComponent implements OnInit {
+
+  totalUsers: number = 0;
+  totalProcess: number = 0;
+
+  users: any[] = [];
+
+  constructor(private userService: UserService, private processService:ProcessService) {}
+
+  ngOnInit(): void {
+    this.fetchTotalUsers();
+    this.fetchTotalProcess();
+  }
+
+  fetchTotalUsers(): void {
+    this.userService.getTotalUsers().subscribe({
+      next: (res) =>this.totalUsers=res.users.length,
+      error: (err) => console.error(err),
+    });
+  }
+
+  fetchTotalProcess(): void {
+    this.processService.getTotalProcess().subscribe({
+      next: (res) =>this.totalUsers=res.processes.length,
+      error: (err) => console.error(err),
+    });
+  }
+
+
 
 }
