fba4f2c96a1bcf1a47e15e589e6e92c73dde218d
Test0-VC <omarabdin1@gmail.com>
2025-04-05 21:00:34 +0200

    routing handel

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { UserListComponent } from './admin/users/user-list/user-list.component';
+import { UserDetailComponent } from './admin/users/user-detail/user-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: UserListComponent },
+    { path: 'admin/users/:id', component: UserDetailComponent }, 
+
+
+];
