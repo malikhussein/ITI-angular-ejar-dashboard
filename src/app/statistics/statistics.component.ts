b78ceacdbbb5951da461be03ee4ecab7b3e7cf30
Mohamedeid602 <eidmedo215@gmail.com>
2025-04-06 00:58:36 +0200

    add the chart of the dashboard

--- src/app/statistics/statistics.component.ts
+++ src/app/statistics/statistics.component.ts
@@ -1,20 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { ProcessService } from '../services/process.service';
+import { Color, NgxChartsModule, ScaleType } from '@swimlane/ngx-charts';
 
 @Component({
+  imports: [NgxChartsModule],
   selector: 'app-dashboard',
   templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.css']
+  styleUrls: ['./statistics.component.css'],
 })
 export class StatisticsComponent implements OnInit {
-
   totalUsers: number = 0;
   totalProcess: number = 0;
+  //user line chart data
+  userLineChartData = [
+    {
+      name: 'Users',
+      series: [
+        { name: 'day1', value: 10 },
+        { name: 'day2', value: 20 },
+        { name: 'day3', value: 25 },
+        { name: 'day4', value: 40 },
+        { name: 'day5', value: 35 },
+        { name: 'day6', value: 50 },
+        { name: 'day7', value: 55 },
+      ],
+    },
+  ];
+  userView: [number, number] = [300, 200];
+
+  //chart data
+  // Sample data for the chart
+  chartData: any[] = [
+    { name: 'Users', value: 800 },
+    { name: 'Processes', value: 150 },
+    { name: 'Products', value: 500 },
+    { name: 'Categories', value: 100 },
+    { name: 'Costs', value: 600 },
+  ];
+
+  view: [number, number] = [1500, 600]; // width, height
+  colorScheme: Color = {
+    name: 'customScheme',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#FFADED', '#562DDD', '#7045FF', '#C768FF', '#B72A67'],
+  };
+  // Show labels on the pie chart
+  updateChartData(): void {
+    this.chartData = [
+      { name: 'Users', value: this.totalUsers },
+      { name: 'Processes', value: this.totalProcess },
+      { name: 'Products', value: 500 },
+      { name: 'Categories', value: 8 },
+      { name: 'Costs', value: 1200 },
+    ];
+  }
 
   users: any[] = [];
 
-  constructor(private userService: UserService, private processService:ProcessService) {}
+  constructor(
+    private userService: UserService,
+    private processService: ProcessService
+  ) {}
 
   ngOnInit(): void {
     this.fetchTotalUsers();
@@ -23,18 +71,20 @@ export class StatisticsComponent implements OnInit {
 
   fetchTotalUsers(): void {
     this.userService.getTotalUsers().subscribe({
-      next: (res) =>this.totalUsers=res.users.length,
+      next: (res) => {
+        (this.totalUsers = res.users.length), this.updateChartData();
+      },
       error: (err) => console.error(err),
     });
   }
 
   fetchTotalProcess(): void {
     this.processService.getTotalProcess().subscribe({
-      next: (res) =>this.totalUsers=res.processes.length,
+      next: (res) => {
+        this.totalUsers = res.processes.length;
+        this.updateChartData();
+      },
       error: (err) => console.error(err),
     });
   }
-
-
-
 }
